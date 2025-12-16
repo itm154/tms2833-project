e38a60fac086d0a8935c8258de0a500f7e013d91
ReinaDNA <xxzhijiexx@gmail.com>
2025-12-16 20:16:56 +0800

    Constructors
    Added constructors for notification and lecturer classes

--- classes/notification.py
+++ classes/notification.py
@@ -1,2 +1,17 @@
 class Notification:
-    pass
+    def __init__(self, notificationID, type, message, generatedDate, isRead, receiver):
+        self.__notificationID = notificationID
+        self.__type = type
+        self.__message = message
+        self.__generatedDate = generatedDate
+        self.__isRead = isRead
+        self.__receiver = receiver
+
+    def markAsRead():
+        pass
+
+    def display():
+        pass
+
+    def sendTo(User):
+        pass
