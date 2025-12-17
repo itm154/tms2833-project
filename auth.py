603d0ccc5015dd70fadaa64612cb293590e50e8b
itm154 <ashrulfahmi@gmail.com>
2025-12-17 20:38:15 +0800

    Update pickle file name

--- auth.py
+++ auth.py
@@ -62,13 +62,13 @@ def save_user(user: User):
     if not os.path.exists(DATA_DIR):
         os.makedirs(DATA_DIR)
 
-    file_path = os.path.join(DATA_DIR, f"{user.getUserName()}.pkl")
+    file_path = os.path.join(DATA_DIR, f"user_{user.getUserName()}.pkl")
     with open(file_path, "wb") as f:
         pickle.dump(user, f)
 
 
 def load_user(username: str) -> User | None:
-    file_path = os.path.join(DATA_DIR, f"{username}.pkl")
+    file_path = os.path.join(DATA_DIR, f"user_{username}.pkl")
     if os.path.exists(file_path):
         with open(file_path, "rb") as f:
             return pickle.load(f)
