9781d55b265fc276c4bd2aca7f1358be49981b0c
itm154 <ashrulfahmi@gmail.com>
2025-12-20 16:41:05 +0800

    Move group CR to student class

--- groupManager.py
+++ groupManager.py
@@ -1,60 +1,8 @@
-import os
+from classes import Group
 import pickle
-
-import auth
-import ui
-from classes import Group, Student
+import os
 
 DATA_DIR = "data"
-
-
-def createGroup(authenticatedUser: Student):
-    # Prompt Group Details
-    name = str(input("Please enter Group Name: "))
-    id = ui.numeric_input("Please enter Group ID: ")
-
-    newGroup = Group(name, id, authenticatedUser.getUserName())
-    save_group(newGroup)
-    print("Group created successfully.")
-    joinGroup(authenticatedUser, newGroup.getGroupID())
-
-
-def joinGroup(authenticatedUser: Student, groupID: int):
-    # Loads the group and adds the student in it
-    temp_group = load_group(groupID)
-
-    if temp_group is None:
-        print("Group not found.")
-        return
-
-    if authenticatedUser.getUserName() in temp_group.getMemberList():
-        print(
-            f"{authenticatedUser.getUserName()} is already a member of {temp_group.getGroupName()}."
-        )
-        return
-
-    temp_group.addMember(authenticatedUser.getUserName())
-    save_group(temp_group)
-    # Loads the student and updates student joined group list
-    authenticatedUser.joinGroup(groupID)
-    auth.save_user(authenticatedUser)
-    print(f"{authenticatedUser.getUserName()} successfully enrolled in group.")
-
-
-def viewGroup(authenticatedUser: Student, groupID: int):
-    temp_group = load_group(groupID)
-    if temp_group is None:
-        print("No groups available.")
-        return
-
-    if authenticatedUser.getUserName() not in temp_group.getMemberList():
-        print("You are not a member of this group.")
-        return
-
-    print(f"Group Name: {temp_group.getGroupName()}")
-    print(f"Leader: {temp_group.getGroupLeader()}")
-    ui.display_list("Members", temp_group.getMemberList())
-    print("Tasks: ")
 
 
 def save_group(group: Group):
--- studentUI.py
+++ studentUI.py
@@ -12,30 +12,27 @@ def studentMenu(authenticatedUser: Student):
         match choice:
             case 1:
                 # Student Creates a Group
-                groupManager.createGroup(authenticatedUser)
+                authenticatedUser.createGroup()
 
             case 2:
                 # Student Joins a Group
                 groupID = ui.numeric_input("Please enter the ID of the group: ")
-                groupManager.joinGroup(authenticatedUser, groupID)
+                authenticatedUser.joinGroup(groupID)
             case 3:
                 # Temporary lists used to fetch and display data
                 groupNames = []
                 id_list = []
                 for groupID in authenticatedUser.getJoinedGroups():
-                    id_list.append(
-                        groupID
-                    )  # Put the list of IDs one by one into the list to match with user choice
                     tempGroup = groupManager.load_group(groupID)
-                    groupNames.append(tempGroup.getGroupName())
+                    if tempGroup:
+                        groupNames.append(tempGroup.getGroupName())
+                        id_list.append(groupID)
                 selected_group = ui.select(
                     "Please Select the Group you wish to visit", groupNames
                 )
 
-                groupManager.viewGroup(authenticatedUser, id_list[selected_group - 1])
-
-                # This function was used to test group adding only, can change the logic of the function to it's intended use.
-                # Student Views an Existing Group
+                if id_list:
+                    authenticatedUser.viewGroup(id_list[selected_group - 1])
             case 4:
                 # Logging out
                 break
