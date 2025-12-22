da1eba71d3236cc3fdc0b9c6ff5733d6ffc5f8dc
itm154 <ashrulfahmi@gmail.com>
2025-12-22 22:11:59 +0800

    Add generate report logic to lecturer UI

--- ui/lecturer_menu.py
+++ ui/lecturer_menu.py
@@ -1,6 +1,7 @@
 import data_manager
 import ui_components
 from classes import Lecturer
+from data_manager import group_manager
 
 
 def lecturerMenu(lecturer: Lecturer):
@@ -16,7 +17,24 @@ def lecturerMenu(lecturer: Lecturer):
         )
         match choice:
             case 1:
-                pass
+                groups = group_manager.loadAllGroups()
+                if not groups:
+                    print("No groups available to generate report.")
+                    continue
+
+                group_names = [group.getGroupName() for group in groups]
+                selected_group_index = ui_components.select(
+                    "Select a group to generate report", group_names
+                )
+                selected_group = groups[selected_group_index - 1]
+
+                report = lecturer.generateReport(selected_group.getGroupID(), 1)
+                if isinstance(report, dict):
+                    ui_components.displayDict(
+                        f"{selected_group.getGroupName()} group report", report
+                    )
+                else:
+                    print(report)
             case 2:
                 pass
             case 3:
